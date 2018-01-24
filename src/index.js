ca468dec1a2557f52c9249f9fcd41f4fde72005d
moyuanhui <moyhui@163.com>
2018-01-24 22:04:49 +0800

    修改首页

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ Page({
       })
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
+      // 此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
