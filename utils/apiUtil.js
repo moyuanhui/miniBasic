294faa1a264d6eccc120f1d3e028aeb58e60ffbd
moyuanhui <moyhui@163.com>
2018-02-12 17:20:50 +0800

    修改接口

--- utils/apiUtil.js
+++ utils/apiUtil.js
@@ -2,7 +2,7 @@ let host = "localhost:288"
 let config = {
     host,
     // 登录地址，用于建立会话
-    getSessionKey: `http://${host}/api/WxMini/GetSessionKey`,
+    getSessionKey: `http://${host}/api/UserInfo/GetUser/Id`,
 }
 
 module.exports = config
